911b018d6e1f9aa57a55494cfac49efd09b47410
mich-hales <michellehales93@gmail.com>
2022-10-21 10:22:13 -0600

    fixed file structure and added some comments to help while we develop

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// TODO: Add a comment describing the functionality of the withAuth middleware
+// TODO: Add a comment describing the functionality of the withAuth middleware:
+// Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
   try {
     const userData = await User.findAll({
@@ -15,6 +16,7 @@ router.get('/', withAuth, async (req, res) => {
     res.render('homepage', {
       users,
       // TODO: Add a comment describing the functionality of this property
+      // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
     });
   } catch (err) {
@@ -24,6 +26,7 @@ router.get('/', withAuth, async (req, res) => {
 
 router.get('/login', (req, res) => {
   // TODO: Add a comment describing the functionality of this if statement
+  // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect('/');
     return;
