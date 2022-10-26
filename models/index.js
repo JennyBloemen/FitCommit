f57d62768bdc53ef3f0048dd1f5f35a246e18629
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-10-26 12:33:34 -0600

    added data, seeding workouts

--- models/index.js
+++ models/index.js
@@ -1,25 +1,25 @@
-const User = require('./User');
-const Schedule = require('./Schedule');
-const Area = require('./Area');
-const Workouts = require('./Workouts');
+const User = require("./User");
+const Schedule = require("./Schedule");
+const Area = require("./Area");
+const Workouts = require("./Workouts");
 
-User.hasMany(Schedule, {    //may need to be changed to hasone
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-    });
-  
+User.hasMany(Schedule, {
+  //may need to be changed to hasone
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
 Schedule.belongsTo(User, {
-    foreignKey: 'user_id'
-    });
+  foreignKey: "user_id",
+});
 
 Area.hasMany(Workouts, {
-    foreignKey: 'area_id',
-    onDelete: 'CASCADE'
-   })
+  foreignKey: "area_id",
+  onDelete: "CASCADE",
+});
 
 Workouts.belongsTo(Area, {
-    foreignKey: 'area_id',
-})
+  foreignKey: "area_id",
+});
 
-module.exports = { User, Schedule };
-  
+module.exports = { User, Schedule, Workouts, Area };
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,16 +1,15 @@
-const sequelize = require('../config/connection');
-const { User } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Workouts } = require("../models");
 
-const userData = require('./userData.json');
+const userData = require("./userData.json");
+const workoutData = require("./workoutData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
+  await User.bulkCreate(userData);
 
+  await Workouts.bulkCreate(workoutData);
   process.exit(0);
 };
 
