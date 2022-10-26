080c7f79f85005bd403684b39e6948dcc7479c08
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-10-26 13:45:55 -0600

    Created seed data for the workout page

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,13 +1,16 @@
 const sequelize = require("../config/connection");
-const { User, Workouts } = require("../models");
+const { User, Workouts, Area } = require("../models");
 
 const userData = require("./userData.json");
 const workoutData = require("./workoutData.json");
+const areaData = require("./areaData.json");
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData);
+
+  await Area.bulkCreate(areaData);
 
   await Workouts.bulkCreate(workoutData);
   process.exit(0);
