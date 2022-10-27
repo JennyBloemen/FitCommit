dfbf8d9429083129ea2090c12fc972a60b70ae9a
mich-hales <michellehales93@gmail.com>
2022-10-26 18:39:08 -0600

    updates to handlebars files and added js files

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-// TODO: Add a comment describing the functionality of the withAuth middleware:
+// mich - remember to add style attribute with specific css layout for each page (style: 'exercises.css') jass.css or schedule.css -- goes in render section within curly brackets
+// also need to change titles for each section (schedule & exercises)
+
+
 // Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
   try {
@@ -15,9 +18,10 @@ router.get('/', withAuth, async (req, res) => {
 
     res.render('homepage', {
       users,
-      // TODO: Add a comment describing the functionality of this property
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
+      title: 'FitCommit',
+      style: 'jass.css',
     });
   } catch (err) {
     res.status(500).json(err);
@@ -25,7 +29,6 @@ router.get('/', withAuth, async (req, res) => {
 });
 
 router.get('/login', (req, res) => {
-  // TODO: Add a comment describing the functionality of this if statement
   // If a session exists, redirect the request to the homepage
   if (req.session.logged_in) {
     res.redirect('/');
