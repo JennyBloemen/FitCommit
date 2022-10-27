7a2e509b9c49508c4e393f0fbf567b3959cfac51
mich-hales <michellehales93@gmail.com>
2022-10-27 15:09:44 -0600

    added helpers to add exercises into exercises.handlebars, not sure if works until we get routes working

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,9 +1,16 @@
+const { options } = require("../models/User");
+
 module.exports = {
   // Will make user input all caps if used
   capitalize: (str) => {
     if (str && typeof str === 'string') {
       return str.toUpperCase();
     } 
+  },
+
+  if_equals: (a, b, options) => {
+    if (a == b)  { return options.fn(this) }
+    return options.inverse(this);
   },
 
   get_emoji: () => {
