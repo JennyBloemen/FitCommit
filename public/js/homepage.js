9ef92c1db2b108aec9bf1be3139ccae0db2bc5f2
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-10-26 21:27:27 -0600

    merging main

---
+++ public/js/homepage.js
@@ -0,0 +1,17 @@
+// const { default: Swal } = require("sweetalert2");
+
+// const getStarted = () => {
+//     if(logged_in === true) {
+//         document.location.replace('/schedule');
+//     } else {
+//         Swal.fire({
+//             title: 'User Access Error',
+//             text: 'You must login or create an account first',
+//             icon: 'error',
+//             color: '#df2c14',
+//             confirmButtonText: 'Got It!'
+//         });
+//     }
+// }
+
+// document.querySelector('.get-started').addEventListener('click', getStarted);
