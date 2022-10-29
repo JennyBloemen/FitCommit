80057f9493c75914eead1239d3ea11a1b5267613
Blackout Shadow <brianhess213@gmail.com>
2022-10-29 15:11:36 -0600

    Updates on the routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -16,33 +16,33 @@ router.get('/', (req, res) => {
 // attempted to edit this route, still not working
 // Route to render the users workout schedule
 router.get('/schedule', withAuth, async (req, res) => {
-try {
-  const scheduleData = await Schedule.findByPk(req.params.user_id, {
-    attributes: { exclude: ['password'] },
-    include: [
-      {
-        model: User,
-        attributes: ['name'],
-        // order: [['name', 'ASC']],
-      }
-    ]
-  });
+// try {
+//   const scheduleData = await Schedule.findByPk(req.params.user_id, {
+//     attributes: { exclude: ['password'] },
+//     include: [
+//       {
+//         model: User,
+//         attributes: ['name'],
+//         // order: [['name', 'ASC']],
+//       }
+//     ]
+//   });
 
-  const schedule = scheduleData.get({ plain: true });
+//   const schedule = scheduleData.get({ plain: true });
 
   res.render('schedule', {
-    ...schedule,
-    logged_in: true,
+    // ...schedule,
+    // logged_in: true,
     title: 'Schedule',
     style: 'schedule.css',
   });
-} catch (err) {
-  res.status(500).json(err);
-}
+// } catch (err) {
+//   res.status(500).json(err);
+// }
 });
 
 
-// Route to render exercises
+// // Route to render exercises
 router.get('/exercises', withAuth, async (req, res) => {
 try {
   const userData = await User.findAll({
@@ -53,7 +53,7 @@ try {
   const users = userData.map((project) => project.get({ plain: true }));
 
   res.render('exercises', {
-    users,
+    User,
     // Pass the logged in flag to the template
     logged_in: req.session.logged_in,
     title: 'Exercises',
@@ -66,10 +66,10 @@ try {
 
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
-  if (req.session.logged_in) {
-    res.redirect('/');
-    return;
-  }
+  // if (req.session.logged_in) {
+  //   res.redirect('/');
+  //   return;
+  // }
 
   res.render('login', {
     title: 'Login',
