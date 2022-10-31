4e3b33428ba96aa5a722edadb4aff594f403efc2
Jenny Bloemen <jennyjbloemen@gmail.com>
2022-10-30 20:31:19 -0600

    removed book emojis from helpers.js

--- utils/helpers.js
+++ utils/helpers.js
@@ -12,17 +12,4 @@ module.exports = {
     if (a == b)  { return options.fn(this) }
     return options.inverse(this);
   },
-
-  get_emoji: () => {
-    const randomNum = Math.random();
-    let book = "📗";
-
-    if (randomNum > 0.7) {
-      book = "📘";
-    } else if (randomNum > 0.4) {
-      book = "📙";
-    }
-
-    return `<span for="img" aria-label="book">${book}</span>`;
-  },
 };
